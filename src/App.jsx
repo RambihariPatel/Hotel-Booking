428a2100937ac5e1f78b91c116c0bd7a5f2c619d
Rambihari Patel <rambiharipatel175@gmail.com>
2026-02-04 18:15:24 +0530

    add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, useLocation, Routes } from 'react-router-dom'
+import Home from './Pages/Home';
 
 
 const App = () => {
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
   return (
     <div>
 
-      <Navbar/>
+      {!isOwnerPath && <Navbar/ > }
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element = {<Home/>}/>
+        </Routes>
+
+      </div>
       
     </div>
   )
